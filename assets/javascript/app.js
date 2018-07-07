f3ff75c9bf3812ddfcd2c0b4a1f34ca359a3c43b
Napoleon <wanderofrazgriz@gmail.com>
2018-07-07 16:41:18 -0700

    updated HTML - JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
 
+    //set up variables for game
+
+    var timer = 40;
+
+    var endGame = false;
+
+    var correctCount = 0;
+
+    var wrongCount = 0;
+
+    //!! If extra time, add audio queues here!!
+    // !!
+
 
 
 
