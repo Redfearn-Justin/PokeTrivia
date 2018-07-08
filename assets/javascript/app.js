62433010e0a7ea9b86de82dff0accfa92cc9c74f
Napoleon <wanderofrazgriz@gmail.com>
2018-07-07 20:30:09 -0700

    updated HTML, JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,6 +12,20 @@ $(document).ready(function() {
 
     var userResponse;
 
+    //capturing responses from user input
+
+    var question1Capture =  $("input[name='question1']:checked").val();
+    var question2Capture =  $("input[name='question2']:checked").val();
+    var question3Capture =  $("input[name='question3']:checked").val();
+    var question4Capture =  $("input[name='question4']:checked").val();
+    var question5Capture =  $("input[name='question5']:checked").val();
+    var question6Capture =  $("input[name='question6']:checked").val();
+    var question7Capture =  $("input[name='question7']:checked").val();
+    var question8Capture =  $("input[name='question8']:checked").val();
+    var question9Capture =  $("input[name='question9']:checked").val();
+    var question10Capture = $("input[name='question10']:checked").val();
+
+
     //!! If extra time, add audio queues here!!
     
     // !!
@@ -94,6 +108,169 @@ $(document).ready(function() {
 
     //function for going through responses
 
+    function responseCheck() {
+
+        // question 1
+
+        if (question1Capture === 1) {
+
+            console.log("correct");
+            correctCount++;
+            $("#correctCountDiv").text("Correct Answers: " + correctCount);
+
+        }
+        else {
+
+            console.log("incorrect");
+            wrongCount++;
+            $("#wrongCountDiv").text("Wrong Answers: " + wrongCount);
+        }
+
+        //question 2
+
+        if (question2Capture === 1) {
+
+            console.log("correct");
+            correctCount++;
+            $("#correctCountDiv").text("Correct Answers: " + correctCount);
+
+        }
+        else {
+
+            console.log("incorrect");
+            wrongCount++;
+            $("#wrongCountDiv").text("Wrong Answers: " + wrongCount);
+        }
+
+        //question 3
+
+        if (question3Capture === 1) {
+
+            console.log("correct");
+            correctCount++;
+            $("#correctCountDiv").text("Correct Answers: " + correctCount);
+
+        }
+        else {
+
+            console.log("incorrect");
+            wrongCount++;
+            $("#wrongCountDiv").text("Wrong Answers: " + wrongCount);
+        }
+
+        //question 4
+
+        if (question4Capture === 1) {
+
+            console.log("correct");
+            correctCount++;
+            $("#correctCountDiv").text("Correct Answers: " + correctCount);
+
+        }
+        else {
+
+            console.log("incorrect");
+            wrongCount++;
+            $("#wrongCountDiv").text("Wrong Answers: " + wrongCount);
+        }
+
+        //question 5
+
+        if (question5Capture === 1) {
+
+            console.log("correct");
+            correctCount++;
+            $("#correctCountDiv").text("Correct Answers: " + correctCount);
+
+        }
+        else {
+
+            console.log("incorrect");
+            wrongCount++;
+            $("#wrongCountDiv").text("Wrong Answers: " + wrongCount);
+        }
+
+        //question 6
+
+        if (question6Capture === 1) {
+
+            console.log("correct");
+            correctCount++;
+            $("#correctCountDiv").text("Correct Answers: " + correctCount);
+
+        }
+        else {
+
+            console.log("incorrect");
+            wrongCount++;
+            $("#wrongCountDiv").text("Wrong Answers: " + wrongCount);
+        }
+
+        //question 7
+
+        if (question7Capture === 1) {
+
+            console.log("correct");
+            correctCount++;
+            $("#correctCountDiv").text("Correct Answers: " + correctCount);
+
+        }
+        else {
+
+            console.log("incorrect");
+            wrongCount++;
+            $("#wrongCountDiv").text("Wrong Answers: " + wrongCount);
+        }
+
+        //question 8
+
+        if (question8Capture === 1) {
+
+            console.log("correct");
+            correctCount++;
+            $("#correctCountDiv").text("Correct Answers: " + correctCount);
+
+        }
+        else {
+
+            console.log("incorrect");
+            wrongCount++;
+            $("#wrongCountDiv").text("Wrong Answers: " + wrongCount);
+        }
+
+        //question 9
+
+        if (question9Capture === 1) {
+
+            console.log("correct");
+            correctCount++;
+            $("#correctCountDiv").text("Correct Answers: " + correctCount);
+
+        }
+        else {
+
+            console.log("incorrect");
+            wrongCount++;
+            $("#wrongCountDiv").text("Wrong Answers: " + wrongCount);
+        }
+
+        //question 10
+
+        if (question10Capture === 1) {
+
+            console.log("correct");
+            correctCount++;
+            $("#correctCountDiv").text("Correct Answers: " + correctCount);
+
+        }
+        else {
+
+            console.log("incorrect");
+            wrongCount++;
+            $("#wrongCountDiv").text("Wrong Answers: " + wrongCount);
+        }
+    }
+
     //calling initializeScreen function
 
     initializeScreen();
@@ -110,6 +287,13 @@ $(document).ready(function() {
 
             gameOver();
         }
+
+    });
+
+    $("#submitButton").on("click", function() {
+
+        gameOver();
+
 
     });
 
